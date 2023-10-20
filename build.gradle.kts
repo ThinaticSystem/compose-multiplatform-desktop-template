9d7307371b3ad359119ea85b6966b4863addbe66
ThinaticSystem <git@thinaticsystem.com>
2023-10-21 02:07:32 +0900

    env dep Voyager

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,15 +19,25 @@ dependencies {
     // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
 
-    // Material
+    // UI
+    //// Material
     implementation("org.jetbrains.compose.material3:material3-desktop:1.5.3")
 
-    // Serialization
-    "1.6.0".also { kotlinxSerializationVersion ->
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
-    }
+    // Navigation
+    //// Voyager
+    val voyagerVersion = "1.0.0-rc07"
+    implementation("cafe.adriel.voyager:voyager-navigator:$voyagerVersion")
+    implementation("cafe.adriel.voyager:voyager-bottom-sheet-navigator:$voyagerVersion")
+    implementation("cafe.adriel.voyager:voyager-tab-navigator:$voyagerVersion")
+    implementation("cafe.adriel.voyager:voyager-transitions:$voyagerVersion")
+    // implementation("cafe.adriel.voyager:voyager-koin:$voyagerVersion")
 
-    // Include the Test API
+    // Serialization
+    //// kotlinx.serialization
+    val kotlinxSerializationVersion = "1.6.0"
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
+
+    // Testing
     testImplementation(compose.desktop.uiTestJUnit4)
 }
 
